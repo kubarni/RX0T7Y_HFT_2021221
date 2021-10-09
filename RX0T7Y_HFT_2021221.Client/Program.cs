7b2e023ce9bd456f2afc8b31b45a30abcac5b8b6
kubar <kubar@DESKTOP-D5RB2V5>
2021-10-09 21:23:47 +0200

    Add project files.

---
+++ RX0T7Y_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RX0T7Y_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
