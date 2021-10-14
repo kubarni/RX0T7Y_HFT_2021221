26b252a981e91ad1ecb59e7fe0369105712a6cb5
kubarni <kubarni@stud.uni-obuda.hu>
2021-10-14 15:45:30 +0200

    create the DbContext class nuget packets installed

---
+++ RX0T7Y_HFT_2021221.Data/BookAuthorPublisherDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RX0T7Y_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RX0T7Y_HFT_2021221.Data
+{
+    public class BookAuthorPublisherDbContext : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Publisher> Publishers { get; set; }
+        public BookAuthorPublisherDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"");
+            }
+        }
+
+    }
+}
