eb8138a86ca40ade20e3e5925d14b2badf033e90
kubarni <kubarni@stud.uni-obuda.hu>
2021-10-14 15:17:58 +0200

    class Book, Author, Publisher is done

---
+++ RX0T7Y_HFT_2021221.Models/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RX0T7Y_HFT_2021221.Models
+{
+    public class Author
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string PlaceOfBirth { get; set; }
+
+        [Required]
+        public int YearOfBirth { get; set; }
+
+        [ForeignKey(nameof(Publisher))]
+        public int PublisherId { get; set; }
+    }
+}
---
+++ RX0T7Y_HFT_2021221.Models/Publisher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RX0T7Y_HFT_2021221.Models
+{
+    public class Publisher
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Headquarters { get; set; }
+
+        [Required]
+        public int Income { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Author> Authors { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Book> Books { get; set; }
+
+        public Publisher()
+        {
+            Authors = new HashSet<Author>();
+            Books = new HashSet<Book>();
+        }
+    }
+}
