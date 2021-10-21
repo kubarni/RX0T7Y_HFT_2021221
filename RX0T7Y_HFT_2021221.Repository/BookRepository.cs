46a8fcea84718df30b997ea9d47048ac3ce38372
kubarni <kubarni@stud.uni-obuda.hu>
2021-10-21 11:20:04 +0200

    Repository classes exist BookRepository done

---
+++ RX0T7Y_HFT_2021221.Repository/BookRepository.cs
@@ -0,0 +1,53 @@
+﻿using RX0T7Y_HFT_2021221.Data;
+using RX0T7Y_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RX0T7Y_HFT_2021221.Repository
+{
+    class BookRepository
+    {
+        BookAuthorPublisherDbContext db;
+
+        public BookRepository(BookAuthorPublisherDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+        }
+
+        public Book Read(int id)
+        {
+            return db.Books.FirstOrDefault(t => t.Id == id);
+        }
+
+        public IQueryable<Book> ReadAll()
+        {
+            return db.Books;
+        }
+
+        public void Delete(int id)
+        {
+            db.Remove(Read(id));
+            db.SaveChanges();
+        }
+
+        public void Update(Book book)
+        {
+            var oldbook = Read(book.Id);
+            oldbook.Name = book.Name;
+            oldbook.Length = book.Length;
+            oldbook.Price = book.Price;
+            oldbook.PublisherId = book.PublisherId;
+
+            db.SaveChanges();
+        }
+    }
+}
--- RX0T7Y_HFT_2021221.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RX0T7Y_HFT_2021221.Repository
-{
-    public class Class1
-    {
-    }
-}
