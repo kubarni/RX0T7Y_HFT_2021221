324f07d42ab62ba100b09885c31ac6d4a84b8369
kubarni <kubarni@stud.uni-obuda.hu>
2021-10-21 11:26:29 +0200

    PublisherRepository done

--- RX0T7Y_HFT_2021221.Repository/PublisherRepository.cs
+++ RX0T7Y_HFT_2021221.Repository/PublisherRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RX0T7Y_HFT_2021221.Data;
+using RX0T7Y_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,43 @@ namespace RX0T7Y_HFT_2021221.Repository
 {
     class PublisherRepository
     {
+        BookAuthorPublisherDbContext db;
+
+        public PublisherRepository(BookAuthorPublisherDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(Publisher publisher)
+        {
+            db.Publishers.Add(publisher);
+            db.SaveChanges();
+        }
+
+        public Publisher Read(int id)
+        {
+            return db.Publishers.FirstOrDefault(t => t.Id == id);
+        }
+
+        public IQueryable<Publisher> ReadAll()
+        {
+            return db.Publishers;
+        }
+
+        public void Delete(int id)
+        {
+            db.Remove(Read(id));
+            db.SaveChanges();
+        }
+
+        public void Update(Publisher publisher)
+        {
+            var oldpublisher = Read(publisher.Id);
+            oldpublisher.Name = publisher.Name;
+            oldpublisher.Income = publisher.Income;
+            oldpublisher.Headquarters = publisher.Headquarters;
+
+            db.SaveChanges();
+        }
     }
 }
