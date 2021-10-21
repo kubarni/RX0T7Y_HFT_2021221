56287b1b88b0e6247df9938aaf2e587f9b0de441
kubarni <kubarni@stud.uni-obuda.hu>
2021-10-21 11:31:25 +0200

    AuthorRepository done

--- RX0T7Y_HFT_2021221.Repository/AuthorRepository.cs
+++ RX0T7Y_HFT_2021221.Repository/AuthorRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RX0T7Y_HFT_2021221.Data;
+using RX0T7Y_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,44 @@ namespace RX0T7Y_HFT_2021221.Repository
 {
     class AuthorRepository
     {
+        BookAuthorPublisherDbContext db;
+
+        public AuthorRepository(BookAuthorPublisherDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(Author author)
+        {
+            db.Authors.Add(author);
+            db.SaveChanges();
+        }
+
+        public Author Read(int id)
+        {
+            return db.Authors.FirstOrDefault(t => t.Id == id);
+        }
+
+        public IQueryable<Author> ReadAll()
+        {
+            return db.Authors;
+        }
+
+        public void Delete(int id)
+        {
+            db.Remove(Read(id));
+            db.SaveChanges();
+        }
+
+        public void Update(Author author)
+        {
+            var oldauthor = Read(author.Id);
+            oldauthor.Name = author.Name;
+            oldauthor.PlaceOfBirth = author.PlaceOfBirth;
+            oldauthor.PublisherId = author.PublisherId;
+            oldauthor.YearOfBirth = author.YearOfBirth;
+
+            db.SaveChanges();
+        }
     }
 }
