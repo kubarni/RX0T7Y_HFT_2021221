6f09a4b3ad8453ba8977e078ebfd23f1e41ee50c
kubarni <kubarni@stud.uni-obuda.hu>
2021-11-16 14:37:45 +0100

    AuthorController done

---
+++ RX0T7Y_HFT_2021221.Endpoint/Controllers/AuthorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RX0T7Y_HFT_2021221.Logic;
+using RX0T7Y_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RX0T7Y_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+
+        IAuthorLogic al;
+
+        public AuthorController(IAuthorLogic al)
+        {
+            this.al = al;
+        }
+
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+            return al.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Author Get(int id)
+        {
+            return al.Read(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Author value)
+        {
+            al.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Author value)
+        {
+            al.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            al.Delete(id);
+        }
+
+
+    }
+}
