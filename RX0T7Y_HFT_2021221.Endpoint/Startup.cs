9c4679a933cf4b7995430344e98c421691524b59
kubarni <kubarni@stud.uni-obuda.hu>
2021-11-16 12:10:31 +0100

    endpoint project reset

--- RX0T7Y_HFT_2021221.Endpoint/Startup.cs
+++ RX0T7Y_HFT_2021221.Endpoint/Startup.cs
@@ -12,13 +12,12 @@ namespace RX0T7Y_HFT_2021221.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,10 +29,7 @@ namespace RX0T7Y_HFT_2021221.Endpoint
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
